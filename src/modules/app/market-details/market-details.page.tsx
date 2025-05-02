bfaf908242ad63878f6a32e89bdb216b34eb0d15
MuriloAlvarez <murilo12alvarez@gmail.com.br>
2025-05-01 23:44:03 -0300

    feat: ✨ Aplicando requisição para busca de estabelecimentos e integração com página

--- src/modules/app/market-details/market-details.page.tsx
+++ src/modules/app/market-details/market-details.page.tsx
@@ -1,31 +1,43 @@
 import { Button } from '@/shared/components/button/button';
 import { colors } from '@/shared/styles/colors';
 import { IconArrowLeft, IconMapPin, IconPhone, IconScan, IconTicket, IconToolsKitchen2 } from '@tabler/icons-react-native';
-import { useLocalSearchParams } from 'expo-router';
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { s } from './market-details-style';
+import useMarketDetails from './useMarket-details.hook';
+import { categoriesIcons } from '@/shared/utils/categories-icons';
+import Loading from '@/shared/components/loading';
 
 export default function MarketDetailsPage() {
-  const params = useLocalSearchParams<{ id: string }>();
+  const {
+    methods: { back },
+    state: { detailsMarket, isLoadingMarkets },
+  } = useMarketDetails();
+
+  if (!detailsMarket) {
+    return null;
+  }
+  const Icon = categoriesIcons[detailsMarket.categoryId];
+
   return (
     <View style={{ flex: 1 }}>
-      <TouchableOpacity style={s.buttonBack}>
+      <TouchableOpacity style={s.buttonBack} onPress={() => back()}>
         <IconArrowLeft color={colors.gray[100]} size={18} />
       </TouchableOpacity>
-      <Image source={{}} style={s.image} />
+      <Image source={{ uri: detailsMarket?.cover }} style={s.image} />
       <View style={s.container}>
         <View style={s.content}>
           <View style={s.information}>
             <View style={s.header}>
-              <Text style={s.title}>Titulo</Text>
-              <IconToolsKitchen2 size={24} color={colors.green.base} />
+              <Text style={s.title}>{detailsMarket?.name}</Text>
+
+              <Icon size={24} color={colors.green.base} />
             </View>
 
-            <Text style={s.description}>Descricão Descrição Descrição Descricão Descrição Descricão Descrição Descricão</Text>
+            <Text style={s.description}>{detailsMarket?.description}</Text>
 
             <View style={s.couponsAvailable}>
               <IconTicket color={colors.red.base} size={24} />
-              <Text style={s.numerCoupon}>2</Text>
+              <Text style={s.numerCoupon}>{detailsMarket?.coupons}</Text>
               <Text style={s.titleCoupon}>cupons disponíveis</Text>
             </View>
           </View>
@@ -34,42 +46,55 @@ export default function MarketDetailsPage() {
             <View style={s.details}>
               <View>
                 <Text style={s.titleDetails}>Regulamento</Text>
-                <View style={s.details}>
-                  <Text style={s.descriptionDetails}>Regra 1</Text>
-                  <Text style={s.descriptionDetails}>Regra 1</Text>
-                </View>
+                {isLoadingMarkets ? (
+                  <Loading />
+                ) : (
+                  <View style={s.details}>
+                    {detailsMarket.rules.map((item) => (
+                      <Text key={item.id} style={s.descriptionDetails}>{`\u2022 ${item.description}`}</Text>
+                    ))}
+                  </View>
+                )}
               </View>
 
               <View style={s.separator} />
 
               <View>
                 <Text style={s.titleDetails}>Informações</Text>
-                <View style={s.details}>
-                  <View style={s.contentDetails}>
-                    <IconMapPin size={18} color={colors.gray[500]} />
-                    <Text style={s.descriptionDetails}>Localização</Text>
+                {isLoadingMarkets ? (
+                  <Loading />
+                ) : (
+                  <View style={s.details}>
+                    <View style={s.contentDetails}>
+                      <IconMapPin size={18} color={colors.gray[500]} />
+                      <Text style={s.descriptionDetails}>{detailsMarket.address}</Text>
+                    </View>
+                    <View style={s.contentDetails}>
+                      <IconPhone size={18} color={colors.gray[500]} />
+                      <Text style={s.descriptionDetails}>{detailsMarket.phone}</Text>
+                    </View>
                   </View>
-                  <View style={s.contentDetails}>
-                    <IconPhone size={18} color={colors.gray[500]} />
-                    <Text style={s.descriptionDetails}>Telefone</Text>
-                  </View>
-                </View>
+                )}
               </View>
 
               <View style={s.separator} />
 
               <View>
                 <Text style={s.titleDetails}>Cupons Usados</Text>
-                <View style={s.details}>
-                  <View style={s.contentDetails}>
-                    <IconTicket size={18} color={colors.green.base} />
-                    <Text style={s.descriptionDetails}>Telefone</Text>
+                {isLoadingMarkets ? (
+                  <Loading />
+                ) : (
+                  <View style={s.details}>
+                    <View style={s.contentDetails}>
+                      <IconTicket size={18} color={colors.green.base} />
+                      <Text style={s.descriptionDetails}>Telefone</Text>
+                    </View>
+                    <View style={s.contentDetails}>
+                      <IconTicket size={18} color={colors.green.base} />
+                      <Text style={s.descriptionDetails}>Telefone</Text>
+                    </View>
                   </View>
-                  <View style={s.contentDetails}>
-                    <IconTicket size={18} color={colors.green.base} />
-                    <Text style={s.descriptionDetails}>Telefone</Text>
-                  </View>
-                </View>
+                )}
               </View>
             </View>
           </ScrollView>
--- src/modules/app/market-details/useMarket-details.hook.ts
+++ src/modules/app/market-details/useMarket-details.hook.ts
@@ -1,3 +1,49 @@
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { PlaceProps } from '../places/components/place';
+import { useToastify } from '@/shared/hooks/toastify';
+import { api } from '@/services/api';
+
+type DetailsProps = PlaceProps & {
+  phone: string;
+  categoryId: string;
+  rules: {
+    id: string;
+    description: string;
+  }[];
+};
+
 export default function useMarketDetails() {
-  return {};
+  const { back } = useRouter();
+  const [isLoadingMarkets, setIsLoadingMarkets] = useState(false);
+  const [detailsMarket, setDetailsMarket] = useState<DetailsProps>();
+  const toast = useToastify();
+  const params = useLocalSearchParams<{ id: string }>();
+
+  async function fetchMarkets() {
+    setIsLoadingMarkets(true);
+    try {
+      const { data } = await api.get(`/markets/${params.id}`);
+      setDetailsMarket(data);
+    } catch (error) {
+      console.log(error);
+      toast.methods.alert('error', 'Não foi possível carregar os estabelecimentos.');
+    } finally {
+      setIsLoadingMarkets(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMarkets();
+  }, [params.id]);
+
+  return {
+    methods: {
+      back,
+    },
+    state: {
+      detailsMarket,
+      isLoadingMarkets,
+    },
+  };
 }
